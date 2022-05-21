09cd26ab3705c9309976f3ba868872ec627c6c76
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-05-21 17:45:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import { Link, useHistory } from "react-router-dom";
+import { AppContext } from "./libs/contextLib";
+import "./App.css";
+import Routes from "./Routes";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
+
+  const history = useHistory();
+
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    history.push("/login");
+  }
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      const result = await Auth.currentSession();
+      setUser(result.getIdToken().payload)
+      setToken(result.getIdToken().getJwtToken())
+      console.log(result)
+      userHasAuthenticated(true);
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert("No current user");
+      }
+    }
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    !isAuthenticating && <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        {
+          isAuthenticated ? <>
+            <Link to="/settings">
+              <NavItem>Settings</NavItem>
+            </Link>
+            <NavItem onClick={handleLogout}>Logout</NavItem>
+          </> :
+            <Nav pullRight>
+              <Link to="/signup">
+                Signup
+              </Link>
+              <Link to="/login">
+                Login
+              </Link>
+            </Nav>
+        }
+
+      </Navbar>
+
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated, user, token }}>
+        <Routes />
+      </AppContext.Provider>
+
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,17 @@
+
+export default {
+  s3: {
+    REGION: "us_east_1",
+    BUCKET: "arn:aws:s3:::demo-aws-lambda-dev-attachmentsbucket-19738emw6zr4s"
+  },
+  apiGateway: {
+    REGION: "us_east_1",
+    URL: "https://ljkwtp0nhk.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us_east_1",
+    USER_POOL_ID: "us-east-1_WWb9WhSLl",
+    APP_CLIENT_ID: "2k4a5lv6s439uu0im8cb3l8h2q",
+    IDENTITY_POOL_ID: "us-east-1:aab0d4fb-3bcb-4699-9518-a9dcb01ca97f",
+  }
+};
