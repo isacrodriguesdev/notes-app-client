912f692e3fe122cb7a89154ef61d7ee309c520d9
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-05-24 11:20:46 -0300

    edit config

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@
 const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "clickfiscal-notes-app-uploads"
+    BUCKET: "dev-notes-uploads"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://xsqk6laefj.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://3txrfwx5ni.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_8AO03Q25U",
-    APP_CLIENT_ID: "1sjj10d8qfqsbd88ev560tobi0",
-    IDENTITY_POOL_ID: "us-east-1:cdb6dd13-0e88-413e-be3b-a84e1518129d",
+    USER_POOL_ID: "us-east-1_Kildr8Wh2",
+    APP_CLIENT_ID: "6gj5t94o8hjs22mo19krdgbij9",
+    IDENTITY_POOL_ID: "us-east-1:47933944-e3cc-4d31-b1aa-695c18890b77",
   },
   STRIPE_KEY: "pk_test_6pRNASCoBOKtIshFeQd4XMUh",
 };
