9809bc43e69ca6c15232c5da7a5910e6ee0d1ba6
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-05-25 16:01:54 -0300

    added config prod

--- src/config.js
+++ src/config.js
@@ -14,25 +14,23 @@ const dev = {
     APP_CLIENT_ID: "6gj5t94o8hjs22mo19krdgbij9",
     IDENTITY_POOL_ID: "us-east-1:47933944-e3cc-4d31-b1aa-695c18890b77",
   },
-  STRIPE_KEY: "pk_test_6pRNASCoBOKtIshFeQd4XMUh",
 };
 
 const prod = {
   s3: {
-    REGION: "",
-    BUCKET: ""
+    REGION: "us-east-1",
+    BUCKET: "prod-notes-uploads"
   },
   apiGateway: {
-    REGION: "",
-    URL: ""
+    REGION: "us-east-1",
+    URL: "https://nfb4havzoa.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "",
-    USER_POOL_ID: "",
-    APP_CLIENT_ID: "",
-    IDENTITY_POOL_ID: "",
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_j879e3fSf",
+    APP_CLIENT_ID: "7ru7v8e4s9inlke4prb2sji1n",
+    IDENTITY_POOL_ID: "us-east-1:84c7bb1c-97ba-4da5-bf01-4dcec6bdbded",
   },
-  STRIPE_KEY: "",
 };
 
 const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
